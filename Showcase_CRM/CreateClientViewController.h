64e572a1ed60c02421542f88bddefedddaf621db
shilinfeng <shilinf@umich.edu>
2014-06-12 18:09:27 +0800

    create client UI modification

---
+++ Showcase_CRM/CreateClientViewController.h
@@ -0,0 +1,13 @@
+//
+//  CreateClientViewController.h
+//  Showcase_CRM
+//
+//  Created by Linfeng Shi on 6/12/14.
+//  Copyright (c) 2014 Linfeng Shi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CreateClientViewController : UIViewController
+
+@end
---
+++ Showcase_CRM/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  Showcase_CRM
+//
+//  Created by Linfeng Shi on 6/12/14.
+//  Copyright (c) 2014 Linfeng Shi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UISplitViewController
+
+@end
