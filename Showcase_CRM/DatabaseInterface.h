15b8448d3e5c5e59b3854f5946fa9c56d6bedff5
shilinfeng <shilinf@umich.edu>
2014-06-30 23:38:08 +0800

    remove unused file

---
+++ Showcase_CRM/DatabaseInterface.h
@@ -0,0 +1,13 @@
+//
+//  DatabaseInterface.h
+//  Showcase_CRM
+//
+//  Created by Linfeng Shi on 6/30/14.
+//  Copyright (c) 2014 Linfeng Shi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DatabaseInterface : NSObject
+
+@end
---
+++ Showcase_CRM/DatabaseInterface.m
@@ -0,0 +1,13 @@
+//
+//  DatabaseInterface.m
+//  Showcase_CRM
+//
+//  Created by Linfeng Shi on 6/30/14.
+//  Copyright (c) 2014 Linfeng Shi. All rights reserved.
+//
+
+#import "DatabaseInterface.h"
+
+@implementation DatabaseInterface
+
+@end
