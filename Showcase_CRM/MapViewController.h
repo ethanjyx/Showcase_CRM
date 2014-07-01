00641f683a32486e3744daf3478793fca5315b08
joseph <fangtee@qq.com>
2014-07-02 00:40:03 +0800

    change storyboard, add mapView

---
+++ Showcase_CRM/MapViewController.h
@@ -0,0 +1,13 @@
+//
+//  MapViewController.h
+//  Showcase_CRM
+//
+//  Created by Logic Solutions on 7/2/14.
+//  Copyright (c) 2014 Linfeng Shi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapViewController : UIViewController
+
+@end
