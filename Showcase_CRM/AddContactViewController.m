ad847b9d133831159648060e68ff2bc4db00358b
shilinfeng <shilinf@umich.edu>
2014-07-13 11:02:59 +0800

    updata contact immediately

--- Showcase_CRM/AddContactViewController.m
+++ Showcase_CRM/AddContactViewController.m
@@ -9,6 +9,7 @@
 #import "AddContactViewController.h"
 #import "DatabaseInterface.h"
 #import "Address.h"
+#import "sksViewController.h"
 
 @interface AddContactViewController ()
 
@@ -83,6 +84,14 @@
 - (IBAction)save:(id)sender {
     DatabaseInterface *database = [DatabaseInterface databaseInterface];
     [database addContactWithLastname:lastname.text firstname:firstname.text title:title.text phoneWork:phone_work.text phoneHome:phone_personal.text phoneMobile:mobile_phone.text emailWork:email_work.text emailPersonal:email_personal.text note:note.text country:country.text province:province.text city:city.text street:address.text postcode:postcode.text companyName:company QQ:QQ.text weChat:WeChat.text skype:Skype.text weibo:Weibo.text];
+}
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    if ([segue.identifier isEqualToString:@"saveContact"] || [segue.identifier isEqualToString:@"returnFromContact"]) {
+        sksViewController *localContact = segue.destinationViewController;
+        localContact.company = company;
+    }
 }
 @end
 
