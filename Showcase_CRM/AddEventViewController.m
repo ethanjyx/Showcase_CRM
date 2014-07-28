8f766d8a83874e56989d5c7bafad1cd6d0e6fb06
Yixing jiang <ethanjyx@umich.edu>
2014-07-28 19:48:00 +0800

    picker for date

--- Showcase_CRM/AddEventViewController.m
+++ Showcase_CRM/AddEventViewController.m
@@ -10,11 +10,25 @@
 
 @interface AddEventViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *nameTextField;
+@property (weak, nonatomic) IBOutlet UITextField *addressTextField;
+@property (weak, nonatomic) IBOutlet UITextField *dateTextField;
+@property (weak, nonatomic) IBOutlet UITextField *contextTextField;
 
+- (IBAction)beginEditDateTextField:(id)sender;
+- (void)updateTextField:(id)sender;
+- (NSString *)formatDate:(NSDate *)d;
+
+- (IBAction)saveAddEvent:(id)sender;
+- (IBAction)cancelAddEvent:(id)sender;
+
+@property (nonatomic, retain) NSDate * date;
 
 @end
 
 @implementation AddEventViewController
+
+@synthesize date, dateTextField;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -28,7 +42,8 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    date = [[NSDate alloc] init];
+    dateTextField.text = [self formatDate:date];
 }
 
 - (void)didReceiveMemoryWarning
@@ -47,6 +62,29 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+- (IBAction)beginEditDateTextField:(id)sender {
+    UIDatePicker *datePicker = [[UIDatePicker alloc]init];
+    [datePicker setDate:[NSDate date]];
+    [datePicker addTarget:self action:@selector(updateTextField:) forControlEvents:UIControlEventValueChanged];
+    [self.dateTextField setInputView:datePicker];
+}
+
+-(void)updateTextField:(id)sender
+{
+    UIDatePicker *picker = (UIDatePicker*)self.dateTextField.inputView;
+    self.dateTextField.text = [self formatDate:picker.date];;
+    date = picker.date;
+}
+
+- (NSString *)formatDate:(NSDate *)d
+{
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateStyle:NSDateFormatterShortStyle];
+    [dateFormatter setDateFormat:@"MM'/'dd'/'yyyy"];
+    NSString *formattedDate = [dateFormatter stringFromDate:d];
+    return formattedDate;
+}
 
 - (IBAction)saveAddEvent:(id)sender {
     [self performSegueWithIdentifier:@"saveEvent" sender:nil];
