b4f07d55f01ce69377d3b3022a02725c00d94f53
shilinfeng <shilinf@umich.edu>
2014-08-03 02:23:13 +0800

    add header for empty page

---
+++ Showcase_CRM/EmptyViewController.h
@@ -0,0 +1,13 @@
+//
+//  EmptyViewController.h
+//  Showcase_CRM
+//
+//  Created by Linfeng Shi on 8/3/14.
+//  Copyright (c) 2014 Linfeng Shi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface EmptyViewController : ViewController
+
+@end
